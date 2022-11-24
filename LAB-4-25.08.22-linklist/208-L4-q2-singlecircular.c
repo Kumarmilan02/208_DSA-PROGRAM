fd68dc7dfa091c58e200736f56c0444bf01559ba
Milan <milankumarsahoo2002@gmail.com>
2022-11-24 18:32:16 +0530

    changes are covered

--- LAB-4-25.08.22-linklist/208-L4-q2-singlecircular.c
+++ LAB-4-25.08.22-linklist/208-L4-q2-singlecircular.c
@@ -1,146 +1,143 @@
-#include"stdio.h"
-#include"stdlib.h"
-#include"string.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 struct student
 {
-    int roll;
-    char name[20];
-    struct student *next;
+        int roll;
+        char name[20];
+        struct student *next;
 };
 struct student *createcircular();
 void traverse(struct student *);
-struct student *insertnode(struct student*,int);
-struct student *deletenode(struct student*,int);
+struct student *insertnode(struct student *, int);
+struct student *deletenode(struct student *, int);
 int main()
 {
-    struct student *b;
-    int n,pos;
-    while(n!=4){
-    printf("\n1.createcircularlinklist \n2.insertnode \n3.deletenode \n4.Exit");
-    printf("\nEnter choice: ");
-    scanf("%d",&n);
-    switch(n)
-    {
-        case 1:
-            b=createcircular();
-            printf("\nDISPLAY\n");
-            traverse(b);
-            break;
-        case 2:
-            printf("\nEnter position to insert: ");
-            scanf("%d",&pos);
-            b=insertnode(b,pos);
-            printf("\nDISPLAY\n");
-            traverse(b);
-            break;
-        case 3:
-            printf("\nEnter position to delete: ");
-            scanf("%d",&pos);
-            b=deletenode(b,pos);
-            printf("\nDISPLAY\n");
-            traverse(b);
-            break;
-        case 4:
-            printf("EXIT");
-            break;
-        default:
-            printf("invalid option.");
-    }
-    }
-    return 0;
-
+        struct student *b;
+        int n, pos;
+        while (n != 4)
+        {
+                printf("\n1.createcircularlinklist \n2.insertnode \n3.deletenode \n4.Exit");
+                printf("\nEnter choice: ");
+                scanf("%d", &n);
+                switch (n)
+                {
+                case 1:
+                        b = createcircular();
+                        printf("\nDISPLAY\n");
+                        traverse(b);
+                        break;
+                case 2:
+                        printf("\nEnter position to insert: ");
+                        scanf("%d", &pos);
+                        b = insertnode(b, pos);
+                        printf("\nDISPLAY\n");
+                        traverse(b);
+                        break;
+                case 3:
+                        printf("\nEnter position to delete: ");
+                        scanf("%d", &pos);
+                        b = deletenode(b, pos);
+                        printf("\nDISPLAY\n");
+                        traverse(b);
+                        break;
+                case 4:
+                        printf("EXIT");
+                        break;
+                default:
+                        printf("invalid option.");
+                }
+        }
+        return 0;
 }
 struct student *createcircular()
 {
-        struct student *p,*q,*temp;
+        struct student *p, *q, *temp;
         char x[5];
-        p=(struct student *)malloc(sizeof(struct student)); //create a linklist.
-        temp=p;  //temp refers first block
+        p = (struct student *)malloc(sizeof(struct student)); // create a linklist.
+        temp = p;                                             // temp refers first block
         printf("Enter roll: ");
-        scanf("%d",&p->roll);
+        scanf("%d", &p->roll);
         printf("Enter name: ");
-        scanf("%s",p->name);
-        while(1)
+        scanf("%s", p->name);
+        while (1)
         {
                 printf("Do you continue yes/no");
-                scanf("%s",x);
-                if (strcmp(x,"no")==0)
+                scanf("%s", x);
+                if (strcmp(x, "no") == 0)
                         break;
-                q=(struct student *)malloc(sizeof(struct student));
-                p->next=q; //joint p&q
-                p=q; // p is shifted to q position
+                q = (struct student *)malloc(sizeof(struct student));
+                p->next = q; // joint p&q
+                p = q;       // p is shifted to q position
                 printf("Enter roll: ");
-                scanf("%d",&p->roll);
+                scanf("%d", &p->roll);
                 printf("Enter name: ");
-                scanf("%s",p->name);
-
+                scanf("%s", p->name);
         }
-        p->next=temp; //last node of linklist
-        return temp; //return base address of linklist
+        p->next = temp; // last node of linklist
+        return temp;    // return base address of linklist
 }
 void traverse(struct student *p)
 {
-    struct student *q=p;
-    while(p->next!=q)
-    {
-        printf("%d %s\n",p->roll,p->name);
-        p=p->next;
-    }
-    printf("%d %s\n",p->roll,p->name);
-}
-struct student *insertnode(struct student *p,int pos)
-{
-        struct student *q,*m,*old=p;
-        int i;
-        if(pos==1)
+        struct student *q = p;
+        while (p->next != q)
         {
-                q=(struct student *)malloc(sizeof(struct student));
-                q->next=p;
+                printf("%d %s\n", p->roll, p->name);
+                p = p->next;
+        }
+        printf("%d %s\n", p->roll, p->name);
+}
+struct student *insertnode(struct student *p, int pos)
+{
+        struct student *q, *m, *old = p;
+        int i;
+        if (pos == 1)
+        {
+                q = (struct student *)malloc(sizeof(struct student));
+                q->next = p;
                 printf("Enter roll: ");
-                scanf("%d",&q->roll);
+                scanf("%d", &q->roll);
                 printf("Enter name: ");
-                scanf("%s",q->name);
+                scanf("%s", q->name);
                 return q;
         }
         else
         {
-                for(i=1;i<pos;i++)
+                for (i = 1; i < pos; i++)
                 {
-                        m=p;
-                        p=p->next;
+                        m = p;
+                        p = p->next;
                 }
-                q=(struct student *)malloc(sizeof(struct student));
-                q->next=p;
+                q = (struct student *)malloc(sizeof(struct student));
+                q->next = p;
                 printf("Enter roll: ");
-                scanf("%d",&q->roll);
+                scanf("%d", &q->roll);
                 printf("Enter name: ");
-                scanf("%s",q->name);
-                m->next=q;
-                q->next=p;
+                scanf("%s", q->name);
+                m->next = q;
+                q->next = p;
                 return old;
-
         }
 }
-struct student *deletenode(struct student *p,int pos)
+struct student *deletenode(struct student *p, int pos)
 {
-        struct student *q,*m,*old=p;
+        struct student *q, *m, *old = p;
         int i;
-        if(pos==1)
+        if (pos == 1)
         {
-                q=p->next;
+                q = p->next;
                 free(p);
                 return q;
         }
         else
         {
-                for(i=1;i<pos;i++)
+                for (i = 1; i < pos; i++)
                 {
-                        m=p;
-                        p=p->next;
+                        m = p;
+                        p = p->next;
                 }
-                m->next=p->next;
+                m->next = p->next;
                 free(p);
                 return old;
-
         }
 }
--- LAB-5-08.09.22-Linklist/208-L5-Q4-singlepallidrome.c
+++ LAB-5-08.09.22-Linklist/208-L5-Q4-singlepallidrome.c
@@ -1,40 +1,44 @@
-//WAP TO CHECK A SINGLE LINKLIST IS PALLIDROME OR NOT.
+// WAP TO CHECK A SINGLE LINKLIST IS PALLIDROME OR NOT.
 
 #include <stdio.h>
 #include <stdlib.h>
-struct node 
+struct node
 {
-  int data;
-  struct node *next;
-}*head=NULL;
-void insert(int num) 
+    int data;
+    struct node *next;
+} *head = NULL;
+void insert(int num)
 {
-    struct node* newNode = (struct node*) malloc(sizeof(struct node));
-    newNode->data  = num;
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = num;
     newNode->next = head;
     head = newNode;
 }
- struct node* reverseCopy(struct node *head) 
- {
+struct node *reverseCopy(struct node *head)
+{
     struct node *newHead = NULL, *temp;
-    if (head == NULL) { 
-       printf("Error : Invalid node pointer !!!\n");       
-       return;  
+    if (head == NULL)
+    {
+        printf("Error : Invalid node pointer !!!\n");
+        return;
     }
-     
-    while(head != NULL) {
-        temp = (struct node*) malloc(sizeof(struct node));
+
+    while (head != NULL)
+    {
+        temp = (struct node *)malloc(sizeof(struct node));
         temp->data = head->data;
         temp->next = newHead;
         newHead = temp;
         head = head->next;
     }
-     
+
     return newHead;
 }
- 
-int compareLinkedList(struct node* LLOne, struct node* LLTwo){ 
-    while (LLOne != NULL && LLTwo != NULL) {
+
+int compareLinkedList(struct node *LLOne, struct node *LLTwo)
+{
+    while (LLOne != NULL && LLTwo != NULL)
+    {
         if (LLOne->data != LLTwo->data)
             return 0;
         LLOne = LLOne->next;
@@ -42,37 +46,44 @@ int compareLinkedList(struct node* LLOne, struct node* LLTwo){
     }
     return (LLOne == NULL && LLTwo == NULL);
 }
-void printLinkedList(struct node *nodePtr) {
-  while (nodePtr != NULL) {
-     printf("%d", nodePtr->data);
-     nodePtr = nodePtr->next;
-     if(nodePtr != NULL)
-         printf("-->");
-  }
+void printLinkedList(struct node *nodePtr)
+{
+    while (nodePtr != NULL)
+    {
+        printf("%d", nodePtr->data);
+        nodePtr = nodePtr->next;
+        if (nodePtr != NULL)
+            printf("-->");
+    }
 }
-  
-int main() {
+
+int main()
+{
     struct node *reverseLL;
     int i, nodeCount, temp;
     printf("Enter number of Nodes in Linked List\n");
     scanf("%d", &nodeCount);
     printf("Enter %d integers\n", nodeCount);
-    for(i = 0; i < nodeCount; i++){
+    for (i = 0; i < nodeCount; i++)
+    {
         scanf("%d", &temp);
         insert(temp);
-    } 
-     
+    }
+
     printf("\nLinked List\n");
     printLinkedList(head);
-     
+
     /* Reverse Input Linked List */
     reverseLL = reverseCopy(head);
-     
-    if(compareLinkedList(head, reverseLL)){
+
+    if (compareLinkedList(head, reverseLL))
+    {
         printf("\nPalindrome Linked List\n");
-    } else {
+    }
+    else
+    {
         printf("\nNot a Palindrome Linked List\n");
     }
-     
+
     return 0;
 }
